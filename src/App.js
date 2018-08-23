d901d478c77dd8154c9054bab0ff4c73c36278f7
Mayank Gangwal <mayankgangwal@Mayanks-MacBook-Air.local>
2018-08-23 10:30:12 +0530

    Section 3 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Person from './Person/Person'
-import OtherPerson from './Person/Person'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
+//import OtherPerson from "./Person/Person";
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: "max", age: 28 },
+      { name: "heeralaalji", age: 55 }
+    ]
+  }
+
+  clickButtonHandler = (newValue, newAge) => {
+    console.log('clicked!');
+    this.setState({
+      persons: [
+        { name: newValue, age: newAge },
+        { name: "heeralaalji", age: 55 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,11 +30,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <OtherPerson />
+        <button onClick={this.clickButtonHandler.bind(this, 'heeralalji', 12)}> Name </button>
+        <Person name="mayank" age="25"> Accessing the children property</Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+          click={this.clickButtonHandler.bind(this, "whatever", 33)} />
+
+        {/* <OtherPerson /> */}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,19 @@
-import React, {Component} from 'react'
+import React from "react";
 
-const Person = () => {
-    return <h1> This is a person </h1>
-}
+const Person = props => {
+  return (
+    <div onClick={props.click} >
+      <h1>This is {props.name} who is {props.age} years old</h1>
+      <p> {props.children} </p>
+    </div>
+  );
+};
 
-class OtherPerson extends Component{
-    render(){
-        return (
-            <div>this is a person again</div>
-        );
-    }
-}
+/* class OtherPerson extends Component {
+  render() {
+    return <div>this is a person again</div>;
+  }
+} */
 
-//export default Person;
-export default OtherPerson;
+export default Person;
+//export default OtherPerson;
